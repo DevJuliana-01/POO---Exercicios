dd54261257382eaaa272eec5ab6a5fee0606ad6e
Dev-Juliana01 <devjuliana64@gmail.com>
2026-01-07 12:31:55 -0300

    Implementação da classe Círculo

---
+++ POOBasico/exercicio1/Circulo.java
@@ -0,0 +1,27 @@
+package POOBasico.exercicio1;
+
+
+// Essa classe calcula a área e circunferência de um círculo.
+public class Circulo {
+    private double raio;
+
+    //Construtor
+    public Circulo(double raio){
+        this.raio = raio;
+    }
+
+    public double getRaio(){
+        return this.raio;
+    }
+    public void setRaio(double raio){
+        this.raio = raio;    }
+
+
+    public double calculaArea(){
+        return Math.PI * (Math.pow(raio, 2));
+    }
+
+    public double calculaCircunferencia(){
+        return 2 * Math.PI * raio;
+    }
+}
